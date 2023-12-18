534cad386bdb0c8ef82a0f27ed6020ae9a8436d8
Juliya <juliyaone123@gmail.com>
2023-12-18 05:21:52 +0300

    start-1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/authenticate', (req, res) => {
+app.get('/authenticate', (req, res) => {
   res.send('Hello World1111!');
 
 });
