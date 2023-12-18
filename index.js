43545491a670f3a8afbdd9aa95e697fc2d30148a
Juliya <juliyaone123@gmail.com>
2023-12-18 05:25:19 +0300

    start-2

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/authenticate', (req, res) => {
-  res.send('Hello World1111!');
+// Страница авторизации
+app.get('/v1.0/login', (req, res) => {
+ 
+  // const { client_id, redirect_uri, state } = req.query;
+  // Отображаем форму для ввода логина и пароля
+  res.send(`
+    <form action="./api/users/auth.php" method="post">
 
+      <label for="username">Логин:</label>
+      <input type="text" id="username" name="username"><br>
+      <label for="password">Пароль:</label>
+      <input type="password" id="password" name="password"><br>
+      <input type="submit" value="Войти">
+    </form>
+  `);
 });
 
 app.listen(port, () => {
