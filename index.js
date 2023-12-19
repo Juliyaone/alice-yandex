6ef306aa943bf81441e09253207dd8beab7b27af
Juliya <juliyaone123@gmail.com>
2023-12-19 03:26:32 +0300

    start-10

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/v1.0/login', (req, res) => {
   const { client_id, redirect_uri, state } = req.query;
   // Отображаем форму для ввода логина и пароля
 
-  console.log(client_id, redirect_uri, state);
+  console.log(client_id, redirect_uri, state, response_type);
 
   res.send(`
     <form action="/v1.0/auth" method="post">
@@ -33,7 +33,7 @@ app.get('/v1.0/login', (req, res) => {
 
 app.post('/v1.0/auth', async (req, res) => {
   try {
-    const { username, password, client_id, redirect_uri, state } = req.body;
+    const { username, password, client_id, redirect_uri, state, response_type} = req.body;
     // Отправляем запрос на PHP-сервер для аутентификации
     const response = await axios.post('http://smart.horynize.ru/api/users/auth.php', {
       username,
@@ -47,7 +47,7 @@ app.post('/v1.0/auth', async (req, res) => {
 
             // res.send('Авторизация прошла успешно!');
 
-      const redirectUrl = `${redirect_uri}?client_id=${client_id}&state=${state}&token=${response.data.token}`;
+      const redirectUrl = `${redirect_uri}?client_id=${client_id}&state=${state}&token=${response.data.token}&response_type='code'`;
       res.redirect(redirectUrl);
     } else {
       res.send('Ошибка аутентификации');
