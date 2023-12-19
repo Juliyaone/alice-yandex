9bba6e3da997216363392fb4926c008d944fb859
Juliya <juliyaone123@gmail.com>
2023-12-19 03:37:56 +0300

    start-12

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.get('/v1.0/login', (req, res) => {
   const { client_id, redirect_uri, state } = req.query;
   // Отображаем форму для ввода логина и пароля
 
-  console.log(client_id, redirect_uri, state);
-
   res.send(`
     <form action="/v1.0/auth" method="post">
       <input type="hidden" name="client_id" value="${client_id}">
@@ -33,30 +31,53 @@ app.get('/v1.0/login', (req, res) => {
 
 app.post('/v1.0/auth', async (req, res) => {
   try {
-    const { username, password, client_id, redirect_uri, state} = req.body;
+    const { username, password, client_id, redirect_uri, state } = req.body;
     // Отправляем запрос на PHP-сервер для аутентификации
     const response = await axios.post('http://smart.horynize.ru/api/users/auth.php', {
       username,
       password
     });
 
-    console.log('response', response);
-
     if (response.statusText === 'OK') {
-      // Успешная аутентификация, перенаправляем пользователя
+      // Успешная аутентификация, генерируем код авторизации
+      const authCode = crypto.randomBytes(16).toString('hex'); // Простая генерация кода
 
-            // res.send('Авторизация прошла успешно!');
-
-      const redirectUrl = `${redirect_uri}?client_id=${client_id}&state=${state}&token=${response.data.token}&response_type='code'`;
+      // Перенаправляем пользователя обратно на redirect_uri с кодом авторизации
+      const redirectUrl = `${redirect_uri}?client_id=${client_id}&state=${state}&code=${authCode}`;
       res.redirect(redirectUrl);
     } else {
       res.send('Ошибка аутентификации');
     }
   } catch (error) {
     console.log(error);
-    res.send('Произошла ошибка');
+    res.send('Произошла ошибка при аутентификации');
   }
 });
+
+// Эндпоинт для обмена кода авторизации на токены
+app.post('/v1.0/token', (req, res) => {
+  const { code, client_id, client_secret } = req.body;
+console.log('ляляляля', code, client_id, client_secret);
+  // Проверяем code, client_id и client_secret
+  // Это должно быть реализовано в соответствии с вашей внутренней логикой безопасности
+  // ...
+
+  if (/* проверка прошла успешно */) {
+    // Генерируем токены
+    const accessToken = crypto.randomBytes(32).toString('hex'); // Простая генерация токена доступа
+    const refreshToken = crypto.randomBytes(32).toString('hex'); // Простая генерация refresh токена
+
+    // Отправляем токены обратно
+    res.json({
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expires_in: 3600, // 1 час, к примеру
+    });
+  } else {
+    res.status(400).json({ error: 'Invalid request' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
