6e49cb85a169f2c016ce2c9dac0bb6093ad36125
Juliya <juliyaone123@gmail.com>
2023-12-22 04:25:06 +0300

    start-47

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ let userJwt = '';
 
 // app.use(morgan('dev'));
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
