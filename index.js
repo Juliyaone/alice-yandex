3f4d7f8ee63c0c21f4e2faebe47c9dea646c680d
Juliya <juliyaone123@gmail.com>
2023-12-22 04:50:27 +0300

    start-48

--- index.js
+++ index.js
@@ -106,12 +106,12 @@ app.post('/v1.0/token', async (req, res) => {
     const refreshToken = jwt.sign({ userId: userId }, secretKeyForToken, { expiresIn: '7d' });
 
     // Сохраняем refresh token в базу данных
-      try {
-        await saveRefreshTokenToDatabase(userId, refreshToken);
-      } catch (innerError) {
-        console.error('Error saving refresh token:', innerError);
-        // Обработка ошибки сохранения refresh token
-      }
+      // try {
+      //   await saveRefreshTokenToDatabase(userId, refreshToken);
+      // } catch (innerError) {
+      //   console.error('Error saving refresh token:', innerError);
+      //   // Обработка ошибки сохранения refresh token
+      // }
     res.json({
       access_token: accessToken,
       refresh_token: refreshToken,
@@ -122,68 +122,68 @@ app.post('/v1.0/token', async (req, res) => {
   }
 });
 
-app.post('/v1.0/refresh_token', async (req, res) => {
-  const { refresh_token } = req.body;
-  try {
-    // Проверяем refresh_token
-    const decoded = jwt.verify(refresh_token, secretKeyForToken);
+// app.post('/v1.0/refresh_token', async (req, res) => {
+//   const { refresh_token } = req.body;
+//   try {
+//     // Проверяем refresh_token
+//     const decoded = jwt.verify(refresh_token, secretKeyForToken);
 
-    const tokenIsValid = await checkRefreshTokenInDatabase(decoded.userId, refresh_token);
+//     const tokenIsValid = await checkRefreshTokenInDatabase(decoded.userId, refresh_token);
 
-    if (tokenIsValid) {
-      // Генерируем новый access_token
-      const accessToken = jwt.sign({ userId: decoded.userId }, secretKeyForToken, { expiresIn: '1h' });
+//     if (tokenIsValid) {
+//       // Генерируем новый access_token
+//       const accessToken = jwt.sign({ userId: decoded.userId }, secretKeyForToken, { expiresIn: '1h' });
 
-      res.json({
-        access_token: accessToken,
-        expires_in: 3600, // 1 час
-      });
-    } else {
-      throw new Error('Invalid refresh token');
-    }
-  } catch (error) {
-    res.status(401).json({ error: 'Invalid or expired refresh token' });
-  }
-});
+//       res.json({
+//         access_token: accessToken,
+//         expires_in: 3600, // 1 час
+//       });
+//     } else {
+//       throw new Error('Invalid refresh token');
+//     }
+//   } catch (error) {
+//     res.status(401).json({ error: 'Invalid or expired refresh token' });
+//   }
+// });
 
 
 // Сохраняем рефреш токен в базу
-async function saveRefreshTokenToDatabase(userId, refreshToken) {
-  try {
-    const response = await axios.post('https://smart.horynize.ru/api/users/token_save.php', {
-      userId: Number(userId),
-      tokenYandex: refreshToken
-    });
+// async function saveRefreshTokenToDatabase(userId, refreshToken) {
+//   try {
+//     const response = await axios.post('https://smart.horynize.ru/api/users/token_save.php', {
+//       userId: Number(userId),
+//       tokenYandex: refreshToken
+//     });
 
-    if (response.status !== 200) {
-      throw new Error('Failed to save refresh token');
-    }
+//     if (response.status !== 200) {
+//       throw new Error('Failed to save refresh token');
+//     }
 
-    console.log('Refresh token saved successfully');
-  } catch (error) {
-    console.error('Error saving refresh token:', error);
-    throw error;
-  }
-}
+//     console.log('Refresh token saved successfully');
+//   } catch (error) {
+//     console.error('Error saving refresh token:', error);
+//     throw error;
+//   }
+// }
 
 // Проверяем рефреш токен в базе
-async function checkRefreshTokenInDatabase(userId, refreshToken) {
-  try {
-    const response = await axios.post('https://smart.horynize.ru/api/users/check_refresh_token.php', {
-      userId: Number(userId),
-      tokenYandex: refreshToken
-    });
+// async function checkRefreshTokenInDatabase(userId, refreshToken) {
+//   try {
+//     const response = await axios.post('https://smart.horynize.ru/api/users/check_refresh_token.php', {
+//       userId: Number(userId),
+//       tokenYandex: refreshToken
+//     });
 
-    if (response.data && response.data.valid) {
-      return true;
-    } else {
-      return false;
-    }
-  } catch (error) {
-    console.error('Error checking refresh token:', error);
-    return false; // В случае ошибки считаем токен недействительным
-  }
-}
+//     if (response.data && response.data.valid) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   } catch (error) {
+//     console.error('Error checking refresh token:', error);
+//     return false; // В случае ошибки считаем токен недействительным
+//   }
+// }
 
 
 //Информация об устройствах пользователя
